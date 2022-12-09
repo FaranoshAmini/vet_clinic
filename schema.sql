6a0a848dd54ab4ab8b6791adf2bed248acd63601
Faranosh Amini <101713819+FaranoshAmini@users.noreply.github.com>
2022-12-10 02:39:53 +0430

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,9 +1,11 @@
 /* Database schema to keep the structure of entire database. */
+/*Milestone 01 */
 CREATE TABLE animals (
-    id INT PRIMARY KEY,
+    id INT GENERATED ALWAYS AS IDENTITY,
     name VARCHAR(100),
     date_of_birth date,
     escape_attempts INT,
-    neutered B OOLEAN,
-    weight_kg DECIMAL
+    n eutered BOOLEAN,
+    weight_kg DECIMAL,
+    PRIMARY KEY(id)
 );
