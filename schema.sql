1838c089b0581ee78c8011bf5fb6e54e4bcb1728
Faranosh Amini <101713819+FaranoshAmini@users.noreply.github.com>
2022-12-10 00:49:15 +0430

    Added columns for animal table

--- schema.sql
+++ schema.sql
@@ -1,10 +1,9 @@
 /* Database schema to keep the structure of entire database. */
 CREATE TABLE animals (
-    id INT,
-    nane VARCHAR(100),
+    id INT PRIMARY KEY,
+    name VARCHAR(100),
     date_of_birth date,
     escape_attempts INT,
-    neutered BOOLEAN,
-    weig ht_kg DECIMAL,
-    PRIMARY KEY(id)
+    neutered B OOLEAN,
+    weight_kg DECIMAL
 );
