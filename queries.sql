d640e822dba7fc6f178fcb3890c6853464216280
Faranosh Amini <101713819+FaranoshAmini@users.noreply.github.com>
2022-12-10 12:32:25 +0430

    Display the animals total

--- queries.sql
+++ queries.sql
@@ -48,12 +48,13 @@ SELECT *
 FROM animals;
 ROLLBACK;
 -- TRANSACTION 04
-
 BEGIN;
-
 DELETE FROM animals WHERE TO_CHAR(date_of_birth,'YYYY-MON-DD') > '2022-JAN-01';
 SAVEPOINT SP1;
 UPDATE animals SET weight_kg = weight_kg*-1;
 ROLLBACK TO SAVEPOINT SP1;
 UPDATE animals SET weight_kg = weight_kg*-1 WHERE weight_kg < 0;
 COMMIT;
+
+-- Queries
+SELECT COUNT(*) total_animals FROM animals;
