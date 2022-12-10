2948a1642f184c5fe3e3952cd1df737535394add
Faranosh Amini <101713819+FaranoshAmini@users.noreply.github.com>
2022-12-11 00:08:34 +0430

    Inserted data into species table

--- data.sql
+++ data.sql
@@ -100,3 +100,6 @@ INSERT INTO owners (full_name, age) VALUES ('Melody Pond', 77);
 INSERT INTO owners (full_name, age) VALUES ('Dean Winchester', 14);
 INSERT INTO owners (full_name, age) VALUES ('Jodie Whittaker', 38);
 
+-- Species Information
+INSERT INTO species(name) VALUES ('Digimon');
+INSERT INTO species(name) VALUES ('Pokemon');
