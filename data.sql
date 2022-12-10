f4fc1757c57d2739eb5c3451ba3ec97010581a01
Faranosh Amini <101713819+FaranoshAmini@users.noreply.github.com>
2022-12-11 00:07:16 +0430

    Inserted data into owner table

--- data.sql
+++ data.sql
@@ -91,3 +91,12 @@ INSERT INTO animals (
 VALUES ('Ditto', '2022-05-14', 5, True, 22);
 
 /*Milestone 03*/
+
+-- Owners Information
+INSERT INTO owners (full_name, age) VALUES ('Sam Smith', 34);
+INSERT INTO owners (full_name, age) VALUES ('Jennifer Orwell', 19);
+INSERT INTO owners (full_name, age) VALUES ('Bob', 45);
+INSERT INTO owners (full_name, age) VALUES ('Melody Pond', 77);
+INSERT INTO owners (full_name, age) VALUES ('Dean Winchester', 14);
+INSERT INTO owners (full_name, age) VALUES ('Jodie Whittaker', 38);
+
