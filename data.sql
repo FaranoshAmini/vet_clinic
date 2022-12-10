0f17b6f59ca294cf28a9dedb88306c388c7f20ff
Faranosh Amini <101713819+FaranoshAmini@users.noreply.github.com>
2022-12-10 11:24:21 +0430

    Inserted data into table

--- data.sql
+++ data.sql
@@ -32,3 +32,12 @@ INSERT INTO animals (
         weight_kg
     )
 VALUES ('Devimon', '2017-05-12', 5, True, 11);
+
+/* Milestone 02*/
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Charmander','2020-02-08',0,False,-11);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Plantmon','2021-11-15',5,True,-5.7);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Squirtle','1993-04-02',5,False,1-12.13);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Angemon','2005-06-12',5,True,-45);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Boarmon','2005-06-07',5,True,20.4);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Blossom','1998-10-13',5,True,17);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Ditto','2022-05-14',5,True,22);
