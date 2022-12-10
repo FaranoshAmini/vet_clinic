1f41e57eaec2a55cf147d458d11f57c701fe3b7e
Faranosh Amini <101713819+FaranoshAmini@users.noreply.github.com>
2022-12-11 00:00:24 +0430

    Added foreign key

--- schema.sql
+++ schema.sql
@@ -17,3 +17,5 @@ ADD species varchar(255);
 CREATE TABLE owners(id INT GENERATED ALWAYS AS IDENTITY, full_name   VARCHAR(100), age  INT, PRIMARY KEY(id));
 CREATE TABLE species(id INT GENERATED ALWAYS AS IDENTITY, name VARCHAR(100), PRIMARY KEY(id));
 ALTER TABLE animals DROP COLUMN species;
+ALTER TABLE animals ADD species_id INT;
+ALTER TABLE animals ADD FOREIGN KEY (species_id) REFERENCES species (id);
